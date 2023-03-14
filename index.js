4c181b2b00648daa990780fe261c07a69a617f29
reddyrups <p.reddy@incedoinc.com>
2023-03-14 10:17:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(cors()) // Use this after the variable declaration
+
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Reddy1234@$$',
+  database: 'mom'
+});
+
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL server!');
+});
+
+// Create a new record
+app.post('/api/records', (req, res) => {
+    console.log("---req body", req)
+  const record = { item: req.body.item, ownerName: req.body.ownerName, eta: req.body.eta, comments: req.body.comments };
+  connection.query('INSERT INTO MomDetails SET ?', record, (err, results) => {
+    if (err) throw err;
+    res.send('Record added successfully!');
+  });
+});
+
+// Read all records
+app.get('/api/records', (req, res) => {
+  connection.query('SELECT * FROM MomDetails', (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+
+// Update a record
+app.put('/api/records/:id', (req, res) => {
+    const id = req.params.id;
+    const record = { item: req.body.item, ownerName: req.body.ownerName, eta: req.body.eta, comments: req.body.comments };
+    connection.query('UPDATE records SET ? MomDetails id = ?', [record, id], (err, results) => {
+      if (err) throw err;
+      res.send('Record updated successfully!');
+    });
+  });
+
+
+// Read users related records
+app.get('/api/records/id/:id', (req, res) => {
+    const id = req.params.id;
+    connection.query('SELECT * FROM MomDetails WHERE id = ?', id, (err, results) => {
+      if (err) throw err;
+      res.send(results);
+    });
+  });
+
+// Read users related records
+app.get('/api/records/user/:ownerName', (req, res) => {
+  const ownerName = req.params.ownerName;
+  connection.query('SELECT * FROM MomDetails WHERE ownerName = ?', ownerName, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+
+// Start the server
+app.listen(3001, () => {
+  console.log('Server started on port 3000!');
+});
