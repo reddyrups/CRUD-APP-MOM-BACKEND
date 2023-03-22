9e537cce2a3866cb0be718ce5beff3db6641aaa3
reddyrups <p.reddy@incedoinc.com>
2023-03-22 15:55:51 +0530

    update

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ connection.connect((err) => {
 
 // Create a new record
 app.post('/api/records', (req, res) => {
-    console.log("---req body", req)
-  const record = { item: req.body.item, ownerName: req.body.ownerName, eta: req.body.eta, comments: req.body.comments };
+  console.log("---req body", req)
+  const record = { item: req.body.item, ownerName: req.body.ownerName, eta: req.body.eta, comments: req.body.comments, status: req.body.status };
   connection.query('INSERT INTO MomDetails SET ?', record, (err, results) => {
     if (err) throw err;
     res.send('Record added successfully!');
@@ -45,23 +45,33 @@ app.get('/api/records', (req, res) => {
 
 // Update a record
 app.put('/api/records/:id', (req, res) => {
-    const id = req.params.id;
-    const record = { item: req.body.item, ownerName: req.body.ownerName, eta: req.body.eta, comments: req.body.comments };
-    connection.query('UPDATE records SET ? MomDetails id = ?', [record, id], (err, results) => {
-      if (err) throw err;
-      res.send('Record updated successfully!');
-    });
+  const id = Number(req.params.id);
+  const date = req.body.eta.split("T")
+  const record = { item: req.body.item, ownerName: req.body.ownerName, eta: date[0] + " " + date[1].split(".")[0], comments: req.body.comments, status: req.body.status };
+  connection.query('UPDATE MomDetails SET ? WHERE id = ?', [record, id], (err, results) => {
+    if (err) throw err;
+    res.send('Record updated successfully!');
   });
+});
+
+// Delete a record
+app.delete('/api/records/:id', (req, res) => {
+  const id = Number(req.params.id);
+  connection.query('DELETE FROM MomDetails WHERE id = ?', id, (err, results) => {
+    if (err) throw err;
+    res.send('Record deleted successfully!');
+  });
+});
 
 
 // Read users related records
 app.get('/api/records/id/:id', (req, res) => {
-    const id = req.params.id;
-    connection.query('SELECT * FROM MomDetails WHERE id = ?', id, (err, results) => {
-      if (err) throw err;
-      res.send(results);
-    });
+  const id = req.params.id;
+  connection.query('SELECT * FROM MomDetails WHERE id = ?', id, (err, results) => {
+    if (err) throw err;
+    res.send(results);
   });
+});
 
 // Read users related records
 app.get('/api/records/user/:ownerName', (req, res) => {
